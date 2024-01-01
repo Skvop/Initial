100c632c002ecb889e5c59a55d9f7d66cccdf0aa
nzavoznenko <miqred@gmail.com>
2024-01-01 18:23:57 +0200

    initial commit

---
+++ task 2.py
@@ -0,0 +1,3 @@
+diagonal_AC = int(input('Please, enter first diagonal length'))
+diagonal_BD = int(input('Please, enter second diagonal length'))
+print('Square of rhombus equals = ' + str((diagonal_AC * diagonal_BD)/2))
---
+++ task 3.py
@@ -0,0 +1,15 @@
+request = int(input('Please, enter 4 numbers'))
+
+first = request // 1000
+print(first)
+
+second = request // 100 % 10
+print(second)
+
+third = request % 100 // 10
+print(third)
+
+fourth = request % 10
+print(fourth)
+
+print("Lets multiply : " + str(first * second * third * fourth))
