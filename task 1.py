aeadaf45055266d3aa79aa10a86b9952c5ba001a
nzavoznenko <miqred@gmail.com>
2024-01-01 22:09:13 +0200

    added new feature

---
+++ task 1.py
@@ -0,0 +1,17 @@
+# num_1 = int(input("Please, enter 1st number : "))
+# num_2 = int(input('Please, enter 2nd number : '))
+# num_3 = int(input('Please, enter 3rd number : '))
+material = input('Please, enter 3 numbers through "-" : ')
+material2 = material.split('-')
+material3 = map(int, material2)
+material4 = list(material3)
+
+action = input("Please tell me what operation to do: take out the maximum from the numbers - 'max'; take out the minimum from the numbers - 'min'; or the arithmetic mean - 'mid': ")
+if action == "min":
+    print(min(material4))
+elif action == "max":
+    print(max(material4))
+elif action == "mid":
+    print(sum(material4)/3)
+else:
+    print('Invalid command, please, restart and follow the instruction.')
