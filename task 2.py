15f6c02c226b3724113fde77ca0300454b0268cb
nzavoznenko <miqred@gmail.com>
2024-01-09 23:13:00 +0200

    lesson3 task 1,2,3

--- task 2.py
+++ task 2.py
@@ -1,14 +1,28 @@
 # diagonal_AC = int(input('Please, enter first diagonal length'))
 # diagonal_BD = int(input('Please, enter second diagonal length'))
 # print('Square of rhombus equals = ' + str((diagonal_AC * diagonal_BD)/2))
-material = int(input('Please, enter number of meters to convert: '))
+# material = int(input('Please, enter number of meters to convert: '))
+#
+# task = input('You want to convert in "miles","yards" or "inches"?')
+# if task == 'miles':
+#     print(material*0.00062137)
+# elif task == 'yards':
+#     print(material*1.0936)
+# elif task == 'inches':
+#     print(material * 39.370)
+# else:
+#     print('Not correct request.')
+try:
+    num1 = int(input("First number : "))
+    num2 = int(input("Second number : "))
 
-task = input('You want to convert in "miles","yards" or "inches"?')
-if task == 'miles':
-    print(material*0.00062137)
-elif task == 'yards':
-    print(material*1.0936)
-elif task == 'inches':
-    print(material * 39.370)
-else:
-    print('Not correct request.')
+    if num1 == num2:
+        print(f"{num1} = {num2}")
+    elif num1<num2:
+        print(f"{num1}, {num2}")
+    else:
+        print(f"{num2}, {num1}")
+# except ValueError as error:
+#     raise Exception("Use only integer numbers !")
+except Exception as error:
+    print(f"Error : {error}")
--- task 3.py
+++ task 3.py
@@ -13,3 +13,25 @@
 # print(fourth)
 #
 # print("Lets multiply : " + str(first * second * third * fourth))
+try:
+    num1 = int(input("First number:\n"))
+    num2 = int(input("Second number:\n"))
+    print("Enter mathematical operation:")
+    act = input(" 1. +\n 2. -\n 3. *\n 4. /\n")
+    if act == "+":
+        res = num1 + num2
+        print(f"{num1} + {num2} = {res}")
+    if act == "-":
+        res = num1 - num2
+        print(f"{num1} - {num2} = {res}")
+    if act == "*":
+        res = num1 * num2
+        print(f"{num1} * {num2} = {res}")
+    if act == "/":
+        res = num1 / num2
+        print(f"{num1} / {num2} = {res}")
+    elif act != "+" and act != "-" and act != "*" and act != "/":
+        print("Incorrect command!")
+
+except Exception as error:
+    print(f"Occurred an error : {error}")
